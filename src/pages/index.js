bd30d8054cf1209190a865f5023b8414e0ca14ef
chiba <ryotai2677@gmail.com>
2024-08-18 14:14:12 +0900

    chore: update import path for Demo01 component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { useStaticQuery, graphql } from 'gatsby';
 import Layout from '../components/layout';
-import Demo01 from '../components/demo01';
+import Demo01 from '../components/demo01/demo01';
 
 const IndexPage = () => {
     return (
